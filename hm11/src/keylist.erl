b4804ff828a16fe5d9413aefaaa6e539442aeb7f
Kotov <kotow.an.an@gmail.com>
2023-11-18 18:47:18 +0700

    homework 11 is done

---
+++ hm11/src/keylist.erl
@@ -0,0 +1,126 @@
+-module(keylist).
+
+-behaviour(gen_server).
+
+%% Callbacks
+-export([init/1, handle_call/3, handle_cast/2]).
+
+%% API
+-export([start_link/1, start_monitor/1, stop/1]).
+-export([add/4, is_member/2, take/2, find/2, delete/2, show_list/1, handle_info/2]).
+
+-record(state, {
+    list = []   :: list(#state{}) | [], 
+    counter = 0 :: integer()
+}).
+
+-type start_mon_ret() :: 
+    {ok, {Pid :: pid(), MonRef :: reference()}} | ignore | {error, Reason :: term()}.
+-type start_ret() ::
+    {ok, Pid :: pid()} | ignore | {error, Reason :: term()}.
+
+
+%% API
+
+-spec start_link(atom()) -> start_ret().
+%% @doc Starts a link for the keylist.
+start_link(Name) ->
+    gen_server:start_link({local, Name}, ?MODULE, [], []).
+
+-spec start_monitor(atom()) -> start_mon_ret().
+%% @doc Starts a monitor for the keylist.
+start_monitor(Name) ->
+    gen_server:start_monitor({local, Name}, ?MODULE, [], []).
+
+-spec stop(atom()) -> ok.
+%% @doc Stops the keylist.
+stop(Name) ->
+    gen_server:stop(Name).
+
+-spec add(atom(), any(), any(), any()) -> {ok, integer()}.
+%% @doc Adds a key-value pair to the keylist.
+add(Name, Key, Value, Comment) ->
+    gen_server:call(Name, {self(), add, Key, Value, Comment}).
+
+-spec is_member(atom(), any()) -> {ok, boolean(), integer()}.
+%% @doc Checks if a key is a member of the keylist.  
+is_member(Name, Key) ->
+    gen_server:call(Name, {self(), is_member, Key}). 
+ 
+-spec take(atom(), any()) -> {ok, not_found | {any(), any(), any()}, integer()}.
+%% @doc Takes a key-value pair from the keylist.
+take(Name, Key) ->
+    gen_server:call(Name, {self(), take, Key}).
+
+-spec find(atom(), any()) -> {ok, not_found | {any(), any(), any()}, integer()}.
+%% @doc Finds a key-value pair in the keylist. 
+find(Name, Key) ->
+    gen_server:call(Name, {self(), find, Key}). 
+
+-spec delete(atom(), any()) -> {ok, list(), integer()}.
+%% @doc Deletes a key-value pair from the keylist.
+delete(Name, Key) ->
+    gen_server:call(Name, {self(), delete, Key}). 
+
+-spec show_list(atom()) -> ok.
+%% @doc Shows the list of key-value pairs in the keylist.
+show_list(Name) ->
+    gen_server:cast(Name, {self(), show_list}).     
+
+%% CALLBACK
+
+%% @hidden
+init([]) -> 
+    {ok, #state{}}.
+
+%% @hidden    
+handle_call({From, add, Key, Value, Comment}, _From, #state{list = List, counter = Counter} = State) ->
+    NewState = State#state{list = [{Key, Value, Comment} | List], counter = Counter + 1},
+    From ! {ok, NewState#state.counter},
+    {reply, {ok, NewState#state.counter}, NewState};
+
+handle_call({From, is_member, Key}, _From, #state{list = List, counter = Counter} = State) ->
+    Res = lists:keymember(Key, 1, List),
+    NewState = State#state{counter = Counter + 1},
+    From ! {ok, Res, NewState#state.counter},
+    {reply, {ok, Res, NewState#state.counter}, NewState};
+
+handle_call({From, take, Key}, _From, #state{list = List, counter = Counter} = State) ->
+    Msg = 
+        case lists:keytake(Key, 1, List) of
+            false ->
+                NewState = State#state{counter = Counter + 1},
+                {ok, not_found, NewState#state.counter};
+            {_, Found, NewList} ->
+                NewState = State#state{list = NewList, counter = Counter + 1},
+                {ok, Found, NewState#state.counter}
+        end,
+    From ! Msg,
+    {reply, Msg, NewState};
+
+handle_call({From, find, Key}, _From, #state{list = List, counter = Counter} = State) ->
+    NewState = State#state{counter = Counter + 1},
+    Msg = 
+        case lists:keyfind(Key, 1, List) of
+            false ->
+                {ok, not_found, NewState#state.counter};
+            Res ->
+                {ok, Res, NewState#state.counter}
+        end,
+    From ! Msg,
+    {reply, Msg, NewState};
+
+handle_call({From, delete, Key}, _From, #state{list = List, counter = Counter} = State) ->
+    Res = lists:keydelete(Key, 1, List),
+    NewState = State#state{counter = Counter + 1, list = Res},
+    From ! {ok, Res, NewState#state.counter},
+    {reply, {ok, Res, NewState#state.counter}, NewState}.
+
+%% @hidden
+handle_cast({From, show_list}, State) ->
+    From ! {ok, State},
+    {noreply, State}.
+
+handle_info({added_new_child, Pid, Name}, State) ->
+    io:format("Info from process ~p: new keylist created with Pid = ~p, Name = ~p~n", [self(), Pid, Name]),
+    {noreply, State}.
---
+++ hm11/src/keylist_test.erl
@@ -0,0 +1,63 @@
+-module(keylist_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(KEYNAME1, keylist1).
+-define(KEYNAME2, keylist2).
+
+calc_add_test_() ->
+    {
+      foreach, fun setup/0, fun teardown/1,
+      [
+        fun test_keylist/0,
+        fun test_keylist_mgr/0
+      ]
+    }.
+
+setup() ->
+    keylist_mgr:start(),
+    {ok, Pid1} = keylist_mgr:start_child(#{name => ?KEYNAME1, restart => permanent}),
+    {ok, Pid2} = keylist_mgr:start_child(#{name => ?KEYNAME2, restart => temporary}),
+    #{childs => [{?KEYNAME1, Pid1}, {?KEYNAME2, Pid2}]}.
+
+teardown(State) ->
+    keylist_mgr:stop(),
+    State.
+
+test_keylist() ->
+    ?debugMsg("keylist module testing"),
+    Key1 = 1,    
+    Key2 = 2,
+    Key3 = 3, 
+    Non_existent_key = 4, 
+
+    Value1 = "one",
+    Value2 = "two",
+    Value3 = "three",
+    Comment = "number",
+
+    ?assertEqual({ok, 1}, keylist:add(?KEYNAME1, Key1, Value1, Comment)),
+    ?assertEqual({ok, 2}, keylist:add(?KEYNAME1, Key2, Value2, Comment)),
+    ?assertEqual({ok, 3}, keylist:add(?KEYNAME1, Key3, Value3, Comment)),
+
+    ?assertEqual({ok, true, 4}, keylist:is_member(?KEYNAME1, Key3)),
+    ?assertEqual({ok, false, 5}, keylist:is_member(?KEYNAME1, Non_existent_key)),
+    
+    ?assertEqual({ok, {Key3,Value3,Comment}, 6}, keylist:take(?KEYNAME1, Key3)),
+    ?assertEqual({ok, not_found, 7}, keylist:take(?KEYNAME1, Non_existent_key)),
+
+    ?assertEqual({ok, {Key2,Value2,Comment}, 8}, keylist:find(?KEYNAME1, Key2)),
+    ?assertEqual({ok, not_found, 9}, keylist:find(?KEYNAME1, Non_existent_key)), 
+    
+    ?assertEqual({ok, [{Key1,Value1,Comment}], 10}, keylist:delete(?KEYNAME1, Key2)).
+
+test_keylist_mgr() ->
+    ?debugMsg("keylist_mgr module testing"),
+    exit(whereis(?KEYNAME1), "killing 1st"),
+    %% Delay to wait for the process to restart and update the list
+    timer:sleep(10),
+    exit(whereis(?KEYNAME2), "killing 2nd"),
+    timer:sleep(10),
+    ?assertEqual(undefined, whereis(?KEYNAME2)),
+    ?assertMatch([{?KEYNAME1, _Pid1}], keylist_mgr:get_names()).
+    
