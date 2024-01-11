173cacb1cc7a222fa394da50fbd1b69feacef53f
Kotov <kotow.an.an@gmail.com>
2024-01-11 23:33:20 +0700

    style: in abonent_handler module case changed to handlers

--- hw18/apps/hw18/src/abonent_handler.erl
+++ hw18/apps/hw18/src/abonent_handler.erl
@@ -2,42 +2,43 @@
 
 -export([init/2]).
 
-init(Req, State) ->
-    #{method := Method} = Req,
-    Resp = 
-        case Method of
-            <<"GET">> -> 
-                case cowboy_req:binding(abonent_num, Req, undefined)  of
-                    undefined ->  
-                        cowboy_req:reply(404, Req);
-                    Number ->            
-                        Res = user_db:show_value(binary_to_integer(Number)),
-                        cowboy_req:reply(200, #{
-                            <<"content-type">> => <<"text/plain">>
-                        }, io_lib:format("~p", [Res]), Req)
-                end;
-            <<"DELETE">> -> 
-                case cowboy_req:binding(abonent_num, Req, undefined)  of
-                    undefined ->  
-                        cowboy_req:reply(404, Req);   
-                    Number ->    
-                        Res = user_db:delete_value(binary_to_integer(Number)),
-                        cowboy_req:reply(200, #{
-                            <<"content-type">> => <<"text/plain">>
-                        }, io_lib:format("~p", [Res]), Req)
-                end;
-            <<"POST">> ->
-                {ok, Body, _Req2} = cowboy_req:read_body(Req),
-                case parse_body(Body) of
-                    {error, wrong_data} -> cowboy_req:reply(400, Req); 
-                    {Num, Name} -> 
-                        Res = user_db:add_value(Num, Name),
-                        cowboy_req:reply(201, #{
-                            <<"content-type">> => <<"text/plain">>
-                        }, io_lib:format("~p", [Res]), Req)
-                end
-        end,
-        {ok, Resp, State}.
+init(Req, State) ->   
+    #{method := Method} = Req,     
+    {ok, handle_request(Method, Req), State}.
+
+
+handle_request(<<"GET">>, Req) ->
+    case cowboy_req:binding(person_num, Req, undefined)  of
+        undefined ->  
+            cowboy_req:reply(404, Req);
+        Number ->            
+            Res = user_db:show_value(binary_to_integer(Number)),
+            cowboy_req:reply(200, #{
+                <<"content-type">> => <<"text/plain">>
+            }, io_lib:format("~p", [Res]), Req)
+    end;
+
+handle_request(<<"DELETE">>, Req) ->
+    case cowboy_req:binding(person_num, Req, undefined)  of
+        undefined ->  
+            cowboy_req:reply(404, Req);   
+        Number ->    
+            Res = user_db:delete_value(binary_to_integer(Number)),
+            cowboy_req:reply(200, #{
+                <<"content-type">> => <<"text/plain">>
+            }, io_lib:format("~p", [Res]), Req)
+    end;
+
+handle_request(<<"POST">>, Req) ->
+    {ok, Body, _Req2} = cowboy_req:read_body(Req),
+    case parse_body(Body) of
+        {error, wrong_data} -> cowboy_req:reply(400, Req); 
+        {Num, Name} -> 
+            Res = user_db:add_value(Num, Name),
+            cowboy_req:reply(201, #{
+                <<"content-type">> => <<"text/plain">>
+            }, io_lib:format("~p", [Res]), Req)
+    end.
 
 %%%%%%%%%%%%%%%%%%%% PRIVATE FUNCTIONS %%%%%%%%%%%%%%%%%%%% 
 
