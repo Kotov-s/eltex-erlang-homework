f1f19c205cb1a217a2e84351edb0eed0dbb9c5fc
Kotov <kotow.an.an@gmail.com>
2024-02-04 16:16:43 +0700

    course project is completed

---
+++ course_project/webRTP/apps/webRTP/src/sip_calls_api.erl
@@ -0,0 +1,77 @@
+-module(sip_calls_api).
+
+-export([call/1, call_all/1, start/0]).
+
+-define(PASSWORD, "1234"). 
+
+-type call_answer() :: {integer(), {ok, voice_sent} | {error, term()} | {wrong_get_meta_data, term()}}.
+
+%% @doc Starts nksip service
+-spec start() -> {ok, pid()} | {error, term()}.
+start() ->
+    nksip:start_link(nksip_curse, #{ 
+        sip_local_host  => "localhost", 
+        sip_from => "sip:101@10.0.10.11:5060", 
+        plugins => [nksip_uac_auto_auth], 
+        sip_listen => "sip:all:5060"
+    }).
+
+%% @doc Function that initiates a call with specified UserNumber  
+-spec call(integer()) -> call_answer().
+call(UserNumber) ->
+    nksip_uac:register(nksip_curse, "<sip:"++ integer_to_list(UserNumber) ++"@10.0.20.11:5060;transport=tcp>", 
+        [{sip_pass, ?PASSWORD}, contact, {get_meta, [<<"contact">>]}]),        
+    Medias = [{<<"audio">>, 1080, [{rtpmap, 0, <<"PCMU/8000">>}, <<"sendrecv">>]}],
+    InviteResult = nksip_uac:invite(nksip_curse, "<sip:"++ integer_to_list(UserNumber) ++"@10.0.20.11:5060;transport=tcp>",
+        [{sip_pass, ?PASSWORD}, {body, nksip_sdp:new("auto.nksip", Medias)}, auto_2xx_ack]),
+    FinalResult = 
+        case InviteResult of
+            {ok, 200, [{dialog, DialogID}]} ->
+                Res = nksip_dialog:get_meta(invite_remote_sdp, DialogID),
+                case get_address_and_port(Res) of
+                    {PBX_IP, Port} ->
+                        VoicePath = voice_generation(UserNumber),
+                        StartVoice = "./voice_client " ++ VoicePath ++ " " ++ PBX_IP ++ " " ++ erlang:integer_to_list(Port),
+                        os:cmd(StartVoice),
+                        {ok, voice_sent};
+                    Other ->
+                        logger:warning("Failed to get address and port, because the value of Res (~p) does not match the expected format: {ok, Info}", [Res]), 
+                        Other
+                end;
+            _ ->
+                logger:warning("Unexpected invite result: ~p~n", [InviteResult]), 
+                InviteResult  
+        end,
+    {UserNumber, FinalResult}.
+
+% Function that initiates calls to all subscribers in the list.
+-spec call_all([{atom(), integer(), list()}]) -> [call_answer()] | [].
+call_all(List) ->
+    call_all(List, []).
+
+%%%%%%%%%%%%%%%%%%%% PRIVATE FUNCTIONS %%%%%%%%%%%%%%%%%%%%
+
+voice_generation(UserNumber) ->
+    [{abonents, UserNumber, Username}] = user_db:show_value(UserNumber),
+    CreateVoiceFile = "gtts-cli 'Hello, " ++ Username ++ "' --output priv/voice/generate.wav" ,
+    ConvertVoice = "ffmpeg -i priv/voice/generate.wav -codec:a pcm_mulaw -ar 8000 -ac 1 -af \"volume=20dB\" priv/voice/output.wav -y",
+    Cmd = CreateVoiceFile ++ " && " ++ ConvertVoice,
+    os:cmd(Cmd),
+    "priv/voice/output.wav".
+
+%% @hidden
+call_all([], Answers) -> Answers;
+
+call_all([{_RecordName, UserNumber, _Username}|Rest], Answers) ->
+    call_all(Rest, [call(UserNumber) | Answers]).
+
+%% @hidden
+get_address_and_port({ok, Info}) ->
+    [PortAndAddressInfo] = element(tuple_size(Info), Info),
+    Port = element(3, PortAndAddressInfo),
+    {_, _, BinIP} = element(8, PortAndAddressInfo),
+    PBX_IP = binary:bin_to_list(BinIP),
+    {PBX_IP, Port};
+    
+get_address_and_port({error, Error}) ->
+    {wrong_get_meta_data, Error}.    
---
+++ course_project/webRTP/apps/webRTP/src/user_db.erl
@@ -0,0 +1,103 @@
+-module(user_db).
+
+-export([create_and_start/0, add_value/2, delete_value/1, show_value/1, show_values/0]).
+
+-record(abonents, 
+                {num,   % Number of abonent
+                name}). % Name of abonent
+
+%%% API  
+
+%% @doc Creates mnesia schema, abonent table (if don't exists) and start mnesia
+-spec create_and_start() -> {ok, already_exists | created_and_started}.
+create_and_start() ->
+    mnesia:start(),
+    case mnesia:create_table(abonents, [{disc_only_copies, [node()]}, {attributes, record_info(fields, abonents)}]) of
+        {aborted,{already_exists,abonents}} -> {ok, already_exists};
+        _ -> 
+            mnesia:stop(), 
+            mnesia:create_schema([node()]),
+            mnesia:start(),
+            mnesia:create_table(abonents, [{disc_only_copies, [node()]}, {attributes, record_info(fields, abonents)}]),
+            fill_db_if_json_exists(),
+            {ok, created_and_started}
+    end.
+    
+
+%% @doc Adds given data to abonents table
+-spec add_value(Num::integer(), Name::string()) -> {error, already_exists} | ok | aborted.
+add_value(Num, Name) ->
+    T_res =
+        mnesia:transaction(
+            fun() ->
+                case mnesia:read({abonents, Num}) of
+                    [] -> mnesia:write(#abonents{num = Num, name = Name});
+                    _ -> {error, already_exists}
+                end
+            end
+        ),
+    t_res_to_res(T_res).
+
+%% @doc Deletes data from abonents table by given abonent num
+-spec delete_value(Num::integer()) -> {error, not_found} | ok | aborted.
+delete_value(Num) ->
+    T_res = 
+        mnesia:transaction(
+            fun() ->
+                case mnesia:read({abonents, Num}) of
+                    [#abonents{num = Num, name = _Name}] -> mnesia:delete({abonents, Num});
+                    _ -> {error, not_found}
+                end
+            end
+        ),
+    t_res_to_res(T_res).
+
+%% @doc Shows abonent's table data by given abonent num
+-spec show_value(Num::integer()) -> [tuple()] | []  | aborted.
+show_value(Num) ->
+    T_res = 
+        mnesia:transaction(
+            fun() ->
+                mnesia:read({abonents, Num})
+            end
+        ),
+    t_res_to_res(T_res).
+
+%% @doc Shows all abonent's table data
+-spec show_values() -> [tuple()] | [] | aborted.
+show_values() ->
+    T_res = 
+        mnesia:transaction(
+            fun() ->
+                mnesia:match_object(#abonents{_ = '_'})
+            end
+        ),
+    t_res_to_res(T_res).
+
+%%%%%%%%%%%%%%%%%%%% PRIVATE FUNCTIONS %%%%%%%%%%%%%%%%%%%% 
+
+%% @hidden
+fill_db_if_json_exists() ->
+    PathToJson = "priv/db/abonents.json",
+    case filelib:is_file(PathToJson) of
+        true -> 
+            {ok, JsonData} = file:read_file(PathToJson),
+            ParsedData = jsone:decode(JsonData),
+            fill_db(ParsedData);
+        false ->
+            logger:warning("There is no '~p' file to fill database", [PathToJson])
+    end.
+
+%% @hidden
+fill_db([]) -> ok;
+
+fill_db([#{<<"name">> := BinName, <<"num">> := Number} | Rest]) ->
+    add_value(Number, binary_to_list(BinName)),
+    fill_db(Rest).
+
+%% @hidden
+t_res_to_res(T_res) ->
+    case T_res of
+        {'atomic', Res} -> Res;
+        {'aborted', _Reason} -> aborted
+    end.
